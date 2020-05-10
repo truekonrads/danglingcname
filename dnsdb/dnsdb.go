f94378ab6325b1395bfd68b42ed3cabd41da8875
Konrads Klints <konrads@klints.me>
2020-05-10 12:51:06 +0800

    First stab at DNSDB

---
+++ dnsdb/dnsdb.go
@@ -0,0 +1,54 @@
+package dnsdb
+
+import (
+	"bufio"
+	"encoding/json"
+	"errors"
+	"fmt"
+	//	"io/ioutil"
+	"net/http"
+)
+
+type RRSetAnswer struct {
+	Count     uint
+	TimeFirst uint32
+	TimeLast  uint32
+	RRName    string
+	RRType    string
+	Bailiwick string
+	rdata     []string
+}
+type DNSDBClient struct {
+	ApiKey string
+}
+
+func (dnsdb *DNSDBClient) Lookup(name string, qtype string) ([]RRSetAnswer, error) {
+	client := &http.Client{}
+	var req *http.Request
+	var resp *http.Response
+	var err error
+	url := fmt.Sprintf("https://api.dnsdb.info/lookup/rrset/name/%s/%s/", name, qtype)
+	if req, err = http.NewRequest("GET", url, nil); err != nil {
+		return nil, err
+	}
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("X-API-Key", dnsdb.ApiKey)
+	if resp, err = client.Do(req); err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != 200 {
+		return nil, errors.New(fmt.Sprintf("Unexpected status code '%v': %s", resp.StatusCode, resp.Status))
+	}
+	records := make([]RRSetAnswer, 0)
+	scanner := bufio.NewScanner(resp.Body)
+	for scanner.Scan() {
+		r := RRSetAnswer{}
+		b := []byte(scanner.Text())
+		if err = json.Unmarshal(b, &r); err != nil {
+			return nil, err
+		}
+		records = append(records, r)
+	}
+	return records, nil
+
+}
---
+++ dnsdb/dnsdb_test.go
@@ -0,0 +1,31 @@
+package dnsdb
+
+import (
+	//. "github.com/truekonrads/danglingcname/dnsdb"
+	"os"
+	"testing"
+)
+
+const LOOKUP_DOMAIN string = "golang.org"
+const LOOKUP_QTYPE string = "ANY"
+
+func TestLookup(t *testing.T) {
+	key := os.Getenv("DNSDB_KEY")
+	if len(key) == 0 {
+		t.Skip("DNSDB_KEY not found, skipping test")
+	}
+	dc := DNSDBClient{ApiKey: key}
+	var res []RRSetAnswer
+	var err error
+	if res, err = dc.Lookup(LOOKUP_DOMAIN, LOOKUP_QTYPE); err != nil {
+
+		t.Fatalf("Unable to lookup %s (%s): %v", LOOKUP_DOMAIN, LOOKUP_QTYPE, err)
+	}
+	if len(res) == 0 {
+		t.Fatalf("Zero records found")
+	}
+	if res[0].RRName != LOOKUP_DOMAIN+"." {
+		t.Fatalf("Unexpected record: %v", res[0])
+	}
+	t.Log(res[0])
+}
