aa99d6eaae1b7ab9fad7417baa1f23e62ea33dd2
Konrads Klints <konrads@klints.me>
2020-05-10 13:48:06 +0800

    First decent version

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.14
 require (
 	github.com/fatih/color v1.9.0
 	github.com/miekg/dns v1.1.29
+	github.com/sirupsen/logrus v1.6.0
 )
--- main.go
+++ main.go
@@ -4,8 +4,10 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
-	"github.com/fatih/color"
+	// "github.com/fatih/color"
 	"github.com/miekg/dns"
+	log "github.com/sirupsen/logrus"
+	. "github.com/truekonrads/danglingcname/dnsdb"
 	"io/ioutil"
 	"net/http"
 	"os"
@@ -52,13 +54,6 @@ func lookupRecord(target string, server string, qtype uint16) (result []string, 
 }
 
 type CRTRecord struct {
-	// "issuer_ca_id": 1449,
-	// "issuer_name": "C=US, O=Symantec Corporation, OU=Symantec Trust Network, CN=Symantec Class 3 Secure Server CA - G4",
-	// "name_value": "intra.amr.kpmg.com\nintra.aspac.kpmg.com\nintra.ema.kpmg.com\nintra.qa.kpmg.com\nintra.stg.kpmg.com\npersonal2.kpmg.com\npersonal.dev.kpmg.com\npersonal.qa.kpmg.com\npersonal.stg.ema.kpmg.com\nplatforms.dev.kpmg.com\nplatforms.ema.kpmg.com\nplatforms.qa.kpmg.com\nplatforms.stg.ema.kpmg.com",
-	// "id": 2383181286,
-	// "entry_timestamp": "2020-01-27T14:44:25.937",
-	// "not_before": "2017-09-08T00:00:00",
-	// "not_after": "2018-04-12T23:59:59"
 	Issuer_ca_id    uint
 	Issuer_name     string
 	Name_value      string
@@ -77,8 +72,10 @@ type ProcessingResult struct {
 func processname(name string) ProcessingResult {
 	var res ProcessingResult
 	res.Name = name
-
-	cname, ok := lookupCNAME(name+".", DNSSERVER)
+	if !strings.HasSuffix(name, ".") {
+		name = name + "."
+	}
+	cname, ok := lookupCNAME(name, DNSSERVER)
 	//fmt.Println(cname, ok)
 	if ok == false {
 		s := fmt.Sprintf("Can't process CNAME lookup for %v\n", name)
@@ -89,7 +86,7 @@ func processname(name string) ProcessingResult {
 	if len(cname) > 0 {
 		// it's a CNAME that points to something
 		res.CNAMEPointsTo = cname
-		a, ok := lookupA(name+".", DNSSERVER)
+		a, ok := lookupA(name, DNSSERVER)
 		if ok {
 			res.ARecords = a
 		} else {
@@ -110,8 +107,9 @@ func main() {
 	targetDomain := flag.String("domain", "", "Target domain. (Required)")
 	dnsServer := flag.String("server", DNSSERVER, "DNS Server. (Optional)")
 	numWorkers := flag.Int("workers", 5, "Number of workers (Optional)")
-
-	jsonFile := flag.String("jsonfile", "", "JSON file from which to read results (Optional)")
+	useDNSDB := flag.Bool("dnsdb", false, "Use DNSDB (set DNSDB_KEY env var) (Optional)")
+	usecrt := flag.Bool("use", false, "Use crt.sh as source")
+	// jsonFile := flag.String("jsonfile", "", "JSON file from which to read results (Optional)")
 	flag.Parse()
 	if *targetDomain == "" {
 		flag.PrintDefaults()
@@ -121,26 +119,50 @@ func main() {
 
 	var body []byte
 	var err error
-	fmt.Printf("[+] Targeting %v\n", *targetDomain)
-	if _, err = os.Stat(*jsonFile); err == nil {
-		body, err = ioutil.ReadFile(*jsonFile)
-		fmt.Printf("Sucesfully read %s", *jsonFile)
-	} else {
+	var dnsdb_records []RRSetAnswer
+	var records []CRTRecord
+	log.Debug(fmt.Sprintf("[+] Targeting %v\n", *targetDomain))
+	if !*useDNSDB && !*usecrt {
+		fmt.Printf("Select at least one source\n")
+		return
+	}
+	if *useDNSDB {
+		log.Debug(fmt.Sprintf("Loading data from DNSDB\n"))
+		key := os.Getenv("DNSDB_KEY")
+		if len(key) == 0 {
+			fmt.Println("No DNSDB_KEY found")
+			return
+		}
+		dnsc := DNSDBClient{ApiKey: key}
+
+		dnsdb_records, err = dnsc.Lookup(fmt.Sprintf("*.%s", *targetDomain), "CNAME")
+		if err != nil {
+			fmt.Println("DNSDB", err)
+			return
+		}
+		log.Info(fmt.Sprintf("Sucesfully fetched from DNSDB %s\n", *targetDomain))
+
+	}
+
+	// if _, err = os.Stat(*jsonFile); err == nil {
+	// 	body, err = ioutil.ReadFile(*jsonFile)
+	// 	fmt.Printf("Sucesfully read %s", *jsonFile)
+	// }
+	if *usecrt {
 		url := fmt.Sprintf("https://crt.sh/?q=%s&output=json", *targetDomain)
-		fmt.Printf("Sucesfully fetched from crt.sh %s\n", *targetDomain)
+		log.Info(fmt.Sprintf("Sucesfully fetched from crt.sh %s\n", *targetDomain))
 		var doc *http.Response
 		if doc, err = http.Get(url); err != nil {
 			fmt.Println(err)
 			return
 		}
 		body, err = ioutil.ReadAll(doc.Body)
-	}
-
-	records := make([]CRTRecord, 0)
-	err = json.Unmarshal(body, &records)
-	if err != nil {
-		fmt.Println(err)
-		return
+		records = make([]CRTRecord, 0)
+		err = json.Unmarshal(body, &records)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
 	}
 
 	//fmt.Println(records)
@@ -157,13 +179,14 @@ func main() {
 		for res := range results {
 			if len(res.CNAMEPointsTo) > 0 {
 				if len(res.ARecords) > 0 {
-					color.Blue(fmt.Sprintf("%v: is a CNAME and points to %v and resolves to %v\n",
-						res.Name,
-						strings.Join(res.CNAMEPointsTo, ", "),
-						strings.Join(res.ARecords, ", "),
-					))
+					log.Debug(
+						fmt.Sprintf("%v: is a CNAME and points to %v and resolves to %v\n",
+							res.Name,
+							strings.Join(res.CNAMEPointsTo, ", "),
+							strings.Join(res.ARecords, ", "),
+						))
 				} else {
-					color.Green(fmt.Sprintf("!!! %v: is a CNAME and points to %v and resolves to nothing (%v)\n",
+					log.Info(fmt.Sprintf("!!! %v: is a CNAME and points to %v and resolves to nothing (%v)\n",
 						res.Name,
 						strings.Join(res.CNAMEPointsTo, ", "),
 						strings.Join(res.ARecords, ", "),
@@ -174,7 +197,7 @@ func main() {
 		}
 	}()
 
-	fmt.Printf("Total %v records\n", len(records))
+	log.Debug(fmt.Sprintf("Total %v records received from crt.sh\n", len(records)))
 	for i := 0; i < len(records); i++ {
 		nv := strings.Split(records[i].Name_value, " ")
 		// fmt.Println(nv)
@@ -189,7 +212,26 @@ func main() {
 			}
 		}
 	}
+	log.Debug(fmt.Sprintf("Loaded %v results from crt.sh\n", counter))
+	counter = 0
+	log.Debug(fmt.Sprintf("Total %v records received from dnsdb\n", len(dnsdb_records)))
+	if len(dnsdb_records) > 0 {
+		for _, r := range dnsdb_records {
+			var rrname string
+			if strings.HasSuffix(r.RRName, ".") {
+				rrname = r.RRName[:len(r.RRName)-1]
+			} else {
+				rrname = r.RRName
+			}
+			if _, ok := recmap[rrname]; !ok {
+				recmap[rrname] = true
+				queue <- rrname
+				counter += 1
+			}
+
+		}
+	}
+	log.Debug(fmt.Sprintf("Loaded %v new results from DNSDB\n", counter))
 	close(queue)
-	fmt.Printf("Loaded %v results\n", counter)
 
 }
